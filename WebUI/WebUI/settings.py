f8cf8ed27f9f8ff00836f602deac5b8b884afcc2
Alexandre Bouthinon <bouthinon.alexandre@gmail.com>
2016-02-15 19:38:24 +0100

    feature (WebUI/*) : Création de l'application Django.

--- WebUI/WebUI/settings.py
+++ WebUI/WebUI/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'PysysmonUI',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -104,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
